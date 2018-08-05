8ebd1b1d0aa71c98b59a0f15e6eb9c88d3e7a02f
sebdok <seb.dok@gmail.com>
2018-08-05 10:48:25 +0200

    dodanie pliku person,json

--- src/main/java/com/sda/javafx/Main.java
+++ src/main/java/com/sda/javafx/Main.java
@@ -1,8 +1,10 @@
 package com.sda.javafx;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sda.javafx.cotroller.PersonConroller;
 import com.sda.javafx.cotroller.PersonDetalis;
 import com.sda.javafx.model.Person;
+import com.sda.javafx.model.PersonJSON;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,23 +13,46 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Main extends Application {
+public class Main extends Application  {
 
     private Stage stage;
     private VBox layout;
 
     private ObservableList<Person> personList = FXCollections.observableArrayList();
+    private List<PersonJSON> personJSONList = new ArrayList<PersonJSON>();
 
-    public Main(){
-        personList.add(new Person("Jan", "Kowalski"));
-        personList.add(new Person("Franek", "Lewandowski"));
-        personList.add(new Person("Zbychu", "Dzik"));
-        personList.add(new Person("Marian", "Zysk"));
-        personList.add(new Person("Jan", "Kowalski"));
-        personList.add(new Person("Jan", "Kowalski"));
+
+    public Main() throws IOException {
+        personJSONList.add(new PersonJSON("Jan", "Kowalski"));
+        personJSONList.add(new PersonJSON("Franek", "Lewandowski"));
+        personJSONList.add(new PersonJSON("Zbychu", "Dzik"));
+        personJSONList.add(new PersonJSON("Marian", "Zysk"));
+        personJSONList.add(new PersonJSON("Jan", "Kowalski"));
+        personJSONList.add(new PersonJSON("Jan", "Kowalski"));
+
+
+        ObjectMapper mapper = new ObjectMapper();
+        File filename = new File("person.json");
+        filename.createNewFile();
+        mapper.writeValue(filename, personJSONList);
+
+
+        PersonJSON[] readorders = mapper.readValue(new File("person.json"), PersonJSON[].class);
+
+        for (PersonJSON p : readorders) {
+            System.out.println(p.getName());
+            personList.add(new Person(p.getName(), p.getLastname()));
+
+        }
     }
+
+
+
 
     public Stage getStage() {
         return stage;
